dc45afc01185489449cb9a9300c141a962cea886
naya1228 <jin100y43@gmail.com>
2025-01-06 14:41:33 +0900

    made files

---
+++ scripts/game.ts
@@ -0,0 +1,4 @@
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+const screen = document.querySelector(".screen");
