9cb78ac1aae0e3fade2f5664cce08d7fd4fcbe2e
naya1228 <jin100y43@gmail.com>
2025-01-15 12:09:51 +0900

    making

--- scripts/game.js
+++ scripts/game.js
@@ -5,13 +5,11 @@ const screen = document.querySelector(".screen");
 const bubble = document.querySelector("#bubble");
 const sR = screen.getBoundingClientRect();
 
-bubble.style.transform = "translate(-50px, -50px)";
-
 function moveBubble(sR) {
-	const x = Math.random() * (sR.width - bubble.offsetWidth);
+	const x = bubble.x + Math.random() * (sR.width - bubble.offsetWidth);
 	const y = Math.random() * (sR.height - bubble.offsetHeight);
 	bubble.style.transform = `translate(${x}px, ${y}px)`;
-	console.log("Bubble moved");
+	console.log(`Bubble moved to (${x}px, ${y}px)`);
 }
 
 setInterval(() => moveBubble(sR), 1000);
