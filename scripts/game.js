1dc4375629974d7b0e3412436a89477eef8c347e
naya1228 <jin100y43@gmail.com>
2025-03-05 08:20:29 +0900

    edit querySelector

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header");
 const main = document.querySelector("main");
 const footer = document.querySelector("footer");
-const screen = document.querySelector(".screen");
+const screen = main.querySelector("screen");
 const bubble = document.querySelector("#bubble");
 const sR = screen.getBoundingClientRect();
 
