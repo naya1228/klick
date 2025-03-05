78365359d7461d49dc6b7e04a09141528335b7e0
naya1228 <jin100y43@gmail.com>
2025-03-06 08:05:28 +0900

    resize optimization

--- scripts/game.js
+++ scripts/game.js
@@ -4,16 +4,10 @@ const footer = document.querySelector("footer");
 const screen = main.querySelector(".screen");
 const bubble = document.querySelector("#bubble");
 
-setInterval(() => {
+bubble.style.width = `${screen.width}`
+function resizeScreen(){
 	screen.style.width = `${window.innerWidth - 200}px`;
 	screen.style.height = `${window.innerHeight - 100}px`;
-}, 10);
+}
 
-// function moveBubble(sR) {
-// 	const x = Math.random() * (sR.width - bubble.offsetWidth);
-// 	const y = Math.random() * (sR.height - bubble.offsetHeight);
-// 	bubble.style.transform = `translate(${x}px, ${y}px)`;
-// 	console.log(`Bubble moved to (${x}px, ${y}px)`);
-// }
-
-// setInterval(() => moveBubble(sR), 1000);
+window.addEventListener("resize", resizeScreen);
