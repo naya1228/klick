5025d03e31458f6bf096ef66c389f405dfe769c0
naya1228 <jin100y43@gmail.com>
2025-03-05 09:02:04 +0900

    edit amrkdown

--- scripts/game.js
+++ scripts/game.js
@@ -5,11 +5,12 @@ const screen = main.querySelector("screen");
 const bubble = document.querySelector("#bubble");
 const sR = screen.getBoundingClientRect();
 
-function moveBubble(sR) {
-	const x = Math.random() * (sR.width - bubble.offsetWidth);
-	const y = Math.random() * (sR.height - bubble.offsetHeight);
-	bubble.style.transform = `translate(${x}px, ${y}px)`;
-	console.log(`Bubble moved to (${x}px, ${y}px)`);
-}
 
-setInterval(() => moveBubble(sR), 1000);
+// function moveBubble(sR) {
+// 	const x = Math.random() * (sR.width - bubble.offsetWidth);
+// 	const y = Math.random() * (sR.height - bubble.offsetHeight);
+// 	bubble.style.transform = `translate(${x}px, ${y}px)`;
+// 	console.log(`Bubble moved to (${x}px, ${y}px)`);
+// }
+
+// setInterval(() => moveBubble(sR), 1000);
