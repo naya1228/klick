7f63e3fdc6e2c31c6662e586f17c693a95a1006a
naya1228 <jin100y43@gmail.com>
2025-03-06 15:15:36 +0900

    button func added

--- scripts/game.js
+++ scripts/game.js
@@ -3,10 +3,20 @@ const main = document.querySelector("main");
 const footer = document.querySelector("footer");
 const screen = main.querySelector(".screen");
 const bubble = document.querySelector("#bubble");
+const ul = screen.querySelector("ul");
+const startBtn = ul.getElementById("startBtn");
+const restartBtn = ul.getElementById("restartBtn");
+restartBtn.parentElement.hidden = true;
 
+let playtime = 300;
+
+function StartGame(event) {
+	startBtn.parentElement.hidden = true;
+	restartBtn.parentElement.hidden = false;
+}
 
 resizeScreen();
-function resizeScreen(){
+function resizeScreen() {
 	screen.style.width = `${window.innerWidth * 0.6}px`;
 	screen.style.height = `${window.innerHeight * 0.8}px`;
 }
