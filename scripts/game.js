50f73deced2a3e7a6685906200e7e27e5325e94f
naya1228 <jin100y43@gmail.com>
2025-03-06 11:48:10 +0900

    resized

--- scripts/game.js
+++ scripts/game.js
@@ -6,8 +6,8 @@ const bubble = document.querySelector("#bubble");
 
 bubble.style.width = `${screen.width}`
 function resizeScreen(){
-	screen.style.width = `${window.innerWidth - 200}px`;
-	screen.style.height = `${window.innerHeight - 100}px`;
+	screen.style.width = `${window.innerWidth * 0.6}px`;
+	screen.style.height = `${window.innerHeight * 0.8}px`;
 }
 
 window.addEventListener("resize", resizeScreen);
