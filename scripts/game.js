0b4cf70f7a96d6b1d57285408f25803584fcb12c
naya1228 <jin100y43@gmail.com>
2025-03-06 11:48:40 +0900

    resize

--- scripts/game.js
+++ scripts/game.js
@@ -5,6 +5,8 @@ const screen = main.querySelector(".screen");
 const bubble = document.querySelector("#bubble");
 
 bubble.style.width = `${screen.width}`
+
+resizeScreen();
 function resizeScreen(){
 	screen.style.width = `${window.innerWidth * 0.6}px`;
 	screen.style.height = `${window.innerHeight * 0.8}px`;
