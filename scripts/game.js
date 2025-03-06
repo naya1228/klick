bc7f998c5a53a72a5acbef99db705dbdab29a6ef
naya1228 <jin100y43@gmail.com>
2025-03-06 15:30:13 +0900

    update

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,6 @@ const header = document.querySelector("header");
 const main = document.querySelector("main");
 const footer = document.querySelector("footer");
 const screen = main.querySelector(".screen");
-const bubble = document.querySelector("#bubble");
 const ul = main.querySelector("ul");
 const startBtn = ul.querySelector("#startBtn");
 const restartBtn = ul.querySelector("#restartBtn");
