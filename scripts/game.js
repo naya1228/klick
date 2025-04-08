60c858d6bfce2cb012f6f3fc96cd15c52c6e937e
naya1228 <jin100y43@gmail.com>
2025-04-09 07:46:36 +0900

    test

--- scripts/game.js
+++ scripts/game.js
@@ -2,3 +2,11 @@ const screen = main.querySelector(".screen");
 const ul = main.querySelector("ul");
 const startBtn = ul.querySelector("#startBtn");
 const restartBtn = ul.querySelector("#restartBtn");
+
+let playtime = 300;
+startBtn.addEventListener("click", gameStart);
+restartBtn.addEventListener("click", reStart);
+
+function gameStart(event){
+    
+}
